d307395b871cdf054adc6815a07e289c2b7f393f
kaezar3600 <cesar.contreras.medina@gmail.com>
2023-03-30 23:25:07 -0500

    agregado carpertas de paginas y cat

---
+++ src/pages/about-us.js
@@ -0,0 +1,12 @@
+
+const AboutUsPage = () => {
+  return (
+    <>
+      <h1>ABOUT US</h1>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maiores animi similique modi distinctio illum ipsum velit amet, sit delectus officia, ad ratione necessitatibus cumque corporis ea eos eius aperiam deleniti!</p>
+    </>
+
+  )
+}
+
+export default AboutUsPage
